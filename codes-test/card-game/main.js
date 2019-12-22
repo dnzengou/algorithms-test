dd7bdc994ec1586e4dd872edf11adeed291b7a5b
dnzengou <desire.yavro@gmail.com>
2019-12-22 18:57:30 +0100

    update files

---
+++ codes-test/card-game/main.js
@@ -0,0 +1,15 @@
+/* let card = {
+suit: "clubs",
+rank: "2",
+value: 2
+};
+
+console.log(card); */
+
+class Card {
+    constructor(suit, rank, value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+}
